18e074c8cd4f037c7f5e7d20a42b42eccf4e48f7
Asbjoernemil <asbjoernemil24@gmail.com>
2024-02-12 11:03:28 +0100

    2 routes virker

--- src/main/java/kea/excercise/person/controller/PersonController.java
+++ src/main/java/kea/excercise/person/controller/PersonController.java
@@ -4,11 +4,13 @@ import kea.excercise.person.model.Person;
 import kea.excercise.person.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class PersonController {
@@ -25,4 +27,15 @@ public class PersonController {
         return persons;
 
     }
-}
+
+
+
+    @GetMapping("/persons/{id}")
+    public Person getPerson(@PathVariable int id) {
+        Optional<Person> person = personRepository.findById(id);
+
+        return person.orElse(null);
+    }
+
+    }
+
