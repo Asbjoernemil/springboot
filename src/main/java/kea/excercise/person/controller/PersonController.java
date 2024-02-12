5145a70345c360d44ef389024aa2caaeed37db16
Asbjoernemil <asbjoernemil24@gmail.com>
2024-02-12 11:23:04 +0100

    bedre fejlhåndtering med POST

--- src/main/java/kea/excercise/person/controller/PersonController.java
+++ src/main/java/kea/excercise/person/controller/PersonController.java
@@ -3,10 +3,9 @@ package kea.excercise.person.controller;
 import kea.excercise.person.model.Person;
 import kea.excercise.person.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -36,6 +35,12 @@ public class PersonController {
         Optional<Person> person = personRepository.findById(id);
 
         return ResponseEntity.of(person);
+    }
+
+    @PostMapping("/persons")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Person createPerson(@RequestBody Person person){
+        return personRepository.save(person);
     }
 
     }
